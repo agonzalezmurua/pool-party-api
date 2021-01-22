273b0d067e88de043947951963072c34577c19d6
Agustín González <8430727+agonzalezmurua@users.noreply.github.com>
2021-01-22 20:59:23 -0300

    feat: fixed typo mistakes

--- routes/beatmapsets.js
+++ routes/beatmapsets.js
@@ -1,7 +1,6 @@
 import express from "express";
 
 import validateSchema from "../middlewares/validateSchema.js";
-import BeatmapsetSchema from "../models/database/beatmapset.js";
 import DatabaseController from "../controllers/database.js";
 import OsuController from "../controllers/osu.js";
 
@@ -24,18 +23,25 @@ router.get("/:id", async (req, res, next) => {
     next(error);
   }
 });
-router.post("/", validateSchema(BeatmapsetSchema), async (req, res, next) => {
-  try {
-    const { document } = req;
-    await document.save();
-    res.json(true);
-  } catch (error) {
-    next(error);
+router.post(
+  "/",
+  validateSchema(DatabaseController.beatmapset),
+  async (req, res, next) => {
+    try {
+      const { document } = req;
+      const beatmapset = await OsuController.beatmapset.findById(
+        document.osu_id
+      );
+      await document.save();
+      res.json(new BeatmapsetResponse(beatmapset, document));
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 router.delete("/:id", async (req, res, next) => {
   try {
-    await DatabaseController.findByIdAndRemove(req.params.id);
+    await DatabaseController.beatmapset.findByIdAndRemove(req.params.id);
   } catch (error) {
     next(error);
   }
