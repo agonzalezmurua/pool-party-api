72a12e059bc9ecba35317fc361bad225ff5f4d03
Agustín González <8430727+agonzalezmurua@users.noreply.github.com>
2021-02-07 16:04:10 -0300

    feat: extended connectionstring format for database

--- services/database.configure.js
+++ services/database.configure.js
@@ -2,6 +2,7 @@ import consola from "consola";
 import mongoose from "mongoose";
 import colors from "colors/safe";
 import config from "config";
+import format from "string-format";
 
 import prefixes from "../constants/consola_prefixes";
 
@@ -10,18 +11,23 @@ import prefixes from "../constants/consola_prefixes";
  * @returns {Promise<void>}
  */
 export async function configure() {
-  const connectionUri = `mongodb://${config.get(
-    "database.domain"
-  )}/${config.get("database.name")}`;
+  const template = config.get("database.connection_string");
+  const connectionString = format(template, {
+    config: config.get("database"),
+    env: {
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+    },
+  });
   try {
     consola.debug(
       prefixes.database,
-      "Using string connection",
-      colors.yellow(connectionUri)
+      "Using string template",
+      colors.yellow(template)
     );
     consola.debug(prefixes.database, "Attempting to connect to database");
 
-    await mongoose.connect(connectionUri, {
+    await mongoose.connect(connectionString, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
